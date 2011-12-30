bb582e8d212a4303f8c9d43f1e07006b9f8517d4
David Gageot <david@gageot.net>
2011-12-30 14:03:38 +0100

    Refactor as Service

--- src/main/java/net/gageot/kittenmash/Kittens.java
+++ src/main/java/net/gageot/kittenmash/Kittens.java
@@ -1,5 +1,6 @@
 package net.gageot.kittenmash;
 
+import com.google.common.util.concurrent.*;
 import org.simpleframework.http.*;
 import org.simpleframework.http.core.*;
 import org.simpleframework.transport.connect.*;
@@ -7,17 +8,45 @@ import org.simpleframework.transport.connect.*;
 import java.io.*;
 import java.net.*;
 
-public class Kittens {
+public class Kittens extends AbstractService implements Container {
+	private SocketConnection socketConnection;
+	private final int port;
+
+	public Kittens(int port) {
+		this.port = port;
+	}
+
+	@Override
+	public void handle(Request req, Response resp) {
+		try {
+			resp.getPrintStream().append("Kitten FaceMash").close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 	public static void main(String[] args) throws Exception {
-		new SocketConnection(new Container() {
-			@Override
-			public void handle(Request req, Response resp) {
-				try {
-					resp.getPrintStream().append("Kitten FaceMash").close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}).connect(new InetSocketAddress(8080));
+		new Kittens(8080).startAndWait();
+	}
+
+	@Override
+	protected void doStart() {
+		try {
+			socketConnection = new SocketConnection(this);
+			socketConnection.connect(new InetSocketAddress(port));
+			notifyStarted();
+		} catch (IOException e) {
+			notifyFailed(e);
+		}
+	}
+
+	@Override
+	protected void doStop() {
+		try {
+			socketConnection.close();
+			notifyStopped();
+		} catch (IOException e) {
+			notifyFailed(e);
+		}
 	}
 }
