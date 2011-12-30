53002e01d7c198aaca9b5991e79390306def94d9
David Gageot <david@gageot.net>
2011-12-30 13:56:08 +0100

    Start simplest web server

--- src/main/java/net/gageot/kittenmash/Kittens.java
+++ src/main/java/net/gageot/kittenmash/Kittens.java
@@ -1,7 +1,27 @@
 package net.gageot.kittenmash;
 
+import org.simpleframework.http.*;
+import org.simpleframework.http.core.*;
+import org.simpleframework.transport.connect.*;
+
+import java.io.*;
+import java.net.*;
+
 public class Kittens {
 	public String show() {
 		return "Kitten FaceMash";
+	}
+
+	public static void main(String[] args) throws Exception {
+		new SocketConnection(new Container() {
+			@Override
+			public void handle(Request req, Response resp) {
+				try {
+					resp.getPrintStream().append("Hello").close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}).connect(new InetSocketAddress(8080));
 	}
 }
